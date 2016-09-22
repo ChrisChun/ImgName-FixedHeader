3a6d9525874c04cfe73929d6e78fcf15dd9c667e
ChrisChun <tkp99172@gmail.com>
2016-09-22 17:21:53 +0800

    Update multiFixedHeader.js
    adding comments for each function

--- multiFixedHeader.js
+++ multiFixedHeader.js
@@ -3,24 +3,24 @@ var imageArr = [];
 var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
 console.log(iOS);
 
-$(window).on("load", function() { //Fires when DOM is loaded
+$(window).on("load", function() {   //Fires when DOM is loaded
     getImageSizes();
     window.requestAnimationFrame(tick);
-    $(window).resize(function() { //Fires when window is resized
+    $(window).resize(function() {   //Fires when window is resized
         getImageSizes();
         scrollDetect(imageArr);
     });
 
 });
 
-function getImageSizes() {
+function getImageSizes() {  //get the numbers of images and its outerHeight
     var $items = $(".insurance_item");
     for(var c = 0; c < $items.length; c++){
         imageArr[c] = $($items[c]).outerHeight();  
     }
 }
 
- function scrollDetect(imageArr){
+ function scrollDetect(imageArr){   //show the title header of image once the image is offscreen.
     var $items = $('.header_item');
     for(var c = 0; c < imageArr.length; c++){
         if($window.scrollTop()  >= (imageArr[c] * (c+1)) - $('#fixed-header').outerHeight()){  
@@ -29,10 +29,10 @@ function getImageSizes() {
             $items.eq(c).hide();
         }
     }
-     window.requestAnimationFrame(tick);
+     window.requestAnimationFrame(tick);    
 }
 
-function tick(){
+function tick(){    //prevent the frame lost to make a incorrect calculation
     scrollDetect(imageArr);
 };
 
